3413d2395466726c57647b0acc63b03c5b62d596
dokeyuka <dokeyuka@g.ecc.u-tokyo.ac.jp>
2022-02-21 14:10:11 +0900

    greedy from final

--- 10problems_for_biginner/no9.cpp
+++ 10problems_for_biginner/no9.cpp
@@ -3,12 +3,42 @@ using namespace std;
 
 int main() {
   string S;
-  string T;
   cin >> S;
 
-  if("dream" in S){
+  int N = S.size();
+  string T = "";
+
+  int i = N - 1;
+  while(true){
+    if (S[i] == 'r'){
+       // dreamer or eraser
+       if(S[i-2] == 'm'){
+         T.insert(0, "dreamer");
+         i -= 7;
+       } else {
+         T.insert(0, "eraser");
+         i -= 6;
+       }
+    } else if (S[i] == 'm'){
+      //dream
+      T.insert(0, "dream");
+      i -= 5;
+    } else if (S[i] == 'e'){
+      //erase
+      T.insert(0, "erase");
+      i -= 5;
+    } else {
+      break;
+    }
+
+    if(i <= 0){
+      break;
+    }
+
   }
-
-
-
+  if(S == T){
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
 }
